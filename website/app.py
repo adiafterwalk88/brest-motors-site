e4ede0011c0a1bc30e4d5f5317698f28bc43ae0d
adiafterwalk88 <adiafterwalk@gmail.com>
2026-07-09 13:53:23 +0300

    Update app.py

--- website/app.py
+++ website/app.py
@@ -183,3 +183,6 @@ def api_stats():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
